922b5f13e9657858ddd208bf0089cdf36adb7d59
Shreedeep Datta <shreedeepdatta@gmail.com>
2024-10-31 20:29:00 +0530

    understanding hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
-function App() {
+import { useState } from 'react'
+import React from 'react'
+import './App.css'
 
+function App() {
+  let [count, setCount] = useState(0)
+  const addvalue=() => {
+    if(count==20){
+      return
+    }
+    count=count+1
+    setCount(count)
+  }
+  const removevalue=()=>{
+    if(count==0)
+      return
+    count=count-1
+    setCount(count)
+  }
+  
   return (
-    <h1>
-      Starting React with Vite 
-    </h1>
+    <>
+      <h1>Chai aur code the GOAT</h1>
+      <h2>Counter value: {count}</h2>
+      <button onClick={addvalue}>Add Value</button>
+      <br/>
+      <button onClick={removevalue}>Remove Value</button>
+    </>
   )
 }
 
